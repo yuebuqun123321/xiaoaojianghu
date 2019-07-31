ddd74a2f255b498bd4b73110c98f23e5277641ae
yuebuqun <3084958921@qq.com>
2019-07-31 14:49:02 +0800

    i did it

--- jianfa.java
+++ jianfa.java
@@ -2,5 +2,6 @@ public class JianFa {
 	public static void main(String[] args) {
 		System.out.println("zigong");
 		System.out.println("no no ");
+		System.out.println("you say no i say yes!!!");
 	}
 }
